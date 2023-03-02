34fcf402b13999fe4ca19b7218381fdd473689f5
Wazyre <32106559+Wazyre@users.noreply.github.com>
2023-03-01 18:04:36 -0800

    Add headers

--- web-ui/src/App.js
+++ web-ui/src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  Button,
+  Col,
+  Container,
+  Row,
 
+} from 'react-bootstrap';
+
+import './App.css';
+/*<div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+*/
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Row>
+        <Col>
+          <h3>Research Projects</h3>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <h3>Web Development Projects</h3>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <a href="https://wazyr.itch.io/"
+          >
+            Game Development Projects
+          </a>
+        </Col>
+      </Row>
+      
+      
+    </Container>
+    
   );
 }
 
