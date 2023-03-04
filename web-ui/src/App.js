28795b01a444c714486181c66d1d786d595c8a30
Wazyre <32106559+Wazyre@users.noreply.github.com>
2023-03-04 12:49:24 -0800

    Add More items to page

--- web-ui/src/App.js
+++ web-ui/src/App.js
@@ -6,7 +6,7 @@ import {
 
 } from 'react-bootstrap';
 
-import './App.css';
+import './App.scss';
 /*<div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
@@ -26,22 +26,36 @@ import './App.css';
 */
 function App() {
   return (
-    <Container>
+    <Container className='contentContainer'>
+      <Row>
+        <Col>
+          <h3 style={{"text-align":"center;"}}>Abdulaziz Alabduljalil</h3>
+        </Col>
+      </Row>
       <Row>
         <Col>
           <h3>Research Projects</h3>
+          <ol>
+            <li>PICARD at UCI</li>
+            <li>SCW at CSUN</li>
+          </ol>
         </Col>
       </Row>
       <Row>
         <Col>
           <h3>Web Development Projects</h3>
+          <ol>
+            <li>Direct Aid Academy</li>
+            <li>Direct Aid Scholarship</li>
+          </ol>
         </Col>
       </Row>
       <Row>
         <Col>
+          <h3>Game Development Projects</h3>
           <a href="https://wazyr.itch.io/"
           >
-            Game Development Projects
+            Here
           </a>
         </Col>
       </Row>
