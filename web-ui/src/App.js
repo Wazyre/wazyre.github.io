736401e4269a2a0948a96845ad74428851d57c22
Wazyre <32106559+Wazyre@users.noreply.github.com>
2023-03-04 13:24:37 -0800

    Add education and technical skills

--- web-ui/src/App.js
+++ web-ui/src/App.js
@@ -27,28 +27,70 @@ import './App.scss';
 function App() {
   return (
     <Container className='contentContainer'>
+      <h3 className="centerHeader">Abdulaziz Alabduljalil</h3>
+      <p className="centerHeader"><a href="https://github.com/Wazyre">Github</a> - <a href="https://www.linkedin.com/in/abdulaziz-alabduljalil/">LinkedIn</a></p>
       <Row>
         <Col>
-          <h3 style={{"text-align":"center;"}}>Abdulaziz Alabduljalil</h3>
-          <p><a href="https://github.com/Wazyre">Github</a> - <a href="https://www.linkedin.com/in/abdulaziz-alabduljalil/">LinkedIn</a></p>
+          <h4>Education</h4>
+          <p>B.S. Computer Science and Engineering at University of California, Irvine</p>
+          <p>Currently M.S. Computer Science at University of Oregon</p>
+          <h4>Techincal Skills & Tools</h4>
+        </Col>
+      </Row>
+      <Row className="flexRow">
+        <Col className="listCol">
+          <ul>
+            <li>Arduino,</li>
+            <li>AWS</li>
+            <li>Blender</li>
+            <li>Git</li>
+            <li>Linux</li>
+            <li>Maven</li>
+            <li>Maven</li>
+            <li>SolidWorks</li>
+            <li>Unity</li>
+          </ul>
+        </Col>
+        <Col className="listCol">
+          <ul>
+            <li>C++</li>
+            <li>C</li>
+            <li>Python</li>
+            <li>Java</li>
+            <li>C#</li>
+            <li>SQL</li>
+            <li>PHP</li>
+            <li>HTML</li>
+            <li>React.js</li>
+          </ul>
+        </Col>
+        <Col className="listCol">
+          <ul>
+            <li>JavaScript</li>
+            <li>MongoDB</li>
+            <li>Node.js</li>
+            <li>Assembly</li>
+            <li>CSS</li>
+            <li>LISP</li>
+          </ul>
         </Col>
       </Row>
       <Row>
         <Col>
           <h3>Research Projects</h3>
-          <ol>
+          <ul>
             <li>PICARD at UCI</li>
             <li>SCW at CSUN</li>
-          </ol>
+          </ul>
         </Col>
       </Row>
       <Row>
         <Col>
           <h3>Web Development Projects</h3>
-          <ol>
+          <ul>
             <li>Direct Aid Academy</li>
             <li>Direct Aid Scholarship</li>
-          </ol>
+          </ul>
         </Col>
       </Row>
       <Row>
@@ -56,12 +98,10 @@ function App() {
           <h3>Game Development Projects</h3>
           <a href="https://wazyr.itch.io/"
           >
-            Here
+            Visit Itch.io
           </a>
         </Col>
       </Row>
-      
-      
     </Container>
     
   );
